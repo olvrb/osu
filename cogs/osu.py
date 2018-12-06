830cd580aceee02047892adb810ff0158351f29d
Starwort <tcphone93@gmail.com>
2018-12-06 06:38:51 +0000

    Redo cogs.osu with subcommands

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import requests
-from osuapi import OsuApi, AHConnector
+from osuapi import OsuApi, AHConnector, enums
 import config
 
 
@@ -11,9 +11,29 @@ class Osu:
         self.api = OsuApi(config.osu, connector=AHConnector())
         self.bot = bot
 
-    @commands.command(name="user")
+    @commands.group(invoke_without_command=True)
     async def user(self, ctx, *, name):
         results = await self.api.get_user(name)
+        await ctx.send(f"{results[0].username}")
+
+    @user.command()
+    async def osu(self, ctx, *, name):
+        results = await self.api.get_user(name)
+        await ctx.send(f"{results[0].username}")
+
+    @user.command()
+    async def taiko(self, ctx, *, name):
+        results = await self.api.get_user(name, mode=enums.OsuMode.taiko)
+        await ctx.send(f"{results[0].username}")
+
+    @user.command()
+    async def mania(self, ctx, *, name):
+        results = await self.api.get_user(name, mode=enums.OsuMode.mania)
+        await ctx.send(f"{results[0].username}")
+
+    @user.command()
+    async def catch(self, ctx, *, name):
+        results = await self.api.get_user(name, mode=enums.OsuMode.ctb)
         await ctx.send(f"{results[0].username}")
 
 
