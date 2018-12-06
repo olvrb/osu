4a32bf060edb46dfe32823991b1f240d84a74e66
Starwort <tcphone93@gmail.com>
2018-12-06 06:12:20 +0000

    Generally clean up. Save config.py then gitignore it

--- bot.py
+++ bot.py
@@ -3,29 +3,37 @@
 # Boonk Gang by jacc
 
 # Imports
-import discord, os, discord.ext.commands, os.path
+import discord
+import os
+import os.path
+from discord.ext import commands
+import config
+
 
 def prefix_func(bot, msg):
     return ['osu!']
 
-class pysu(discord.ext.commands.Bot):
+
+class pysu(commands.Bot):
 
     def cog_loads(self):
         return ["cogs." + filename[:-3] for filename in os.listdir('cogs') if os.path.isfile(os.path.join("cogs", filename)) and filename.endswith('.py')]
 
     def __init__(self):
-        self.token = "NDk4MTMwMTE2NzU3Njg0MjM0.DuYTgw.4PI1H10Z3goH_8xMZlS29oCFoWY"
         self.version = 1.0
-        self.osu = "7520b4755cfd7161b3da85be693bea51e49ba412"
         super().__init__(command_prefix=prefix_func)
         self.remove_command("help")
 
         for ext in self.cog_loads():
             print(f"[+] Loaded cog {ext}")
             self.load_extension(ext)
-
-        if __name__=="__main__":
-            super().run(self.token)
+    async def on_ready(self):
+        print('-'*40)
+        print('{:^40}'.format(self.user))
+        print('{:^40}'.format(self.user.id))
+        print('-'*40)
 
 # Runs the bot.
-pysu()
+bot = pysu()
+if __name__ == "__main__":
+    bot.run(config.token)
