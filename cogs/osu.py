326ceb0d3460b06af5915ff138d8fc5b0b804ca5
Starwort <tcphone93@gmail.com>
2018-12-06 06:31:18 +0000

    Async is always better

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,19 +1,19 @@
 import discord
 from discord.ext import commands
 import requests
-from osuapi import OsuApi, ReqConnector
+from osuapi import OsuApi, AHConnector
 import config
 
 
 class Osu:
 
     def __init__(self, bot):
-        self.api = OsuApi(config.osu, connector=ReqConnector())
+        self.api = OsuApi(config.osu, connector=AHConnector())
         self.bot = bot
 
     @commands.command(name="user")
     async def user(self, ctx, *, name):
-        results = self.api.get_user(name)
+        results = await self.api.get_user(name)
         await ctx.send(f"{results[0].username}")
 
 
