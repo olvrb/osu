ca91bff892fd273279eea5f7ebf9909f09a5aad9
Jack <jack.lafond@icloud.com>
2018-12-09 11:28:38 -0500

    i broke some shit

---
+++ cogs/meta.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Configure:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @pysu.command(name = "ping", aliases = ['latency'])
+    async def latency(self, ctx):
+        return await ctx.send(f"⏱ `{round(self.bot.latency*1000)}ms`")
+
+
+def setup(bot):
+    bot.add_cog(Meta(bot))
