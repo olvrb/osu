8d3993a9bdece573bbf68eb3d50ed72e4dd524e1
Starwort <tcphone93@gmail.com>
2018-12-09 07:22:55 +0000

    Add a newline and make the 's' appear based on count

--- cogs/osu.py
+++ cogs/osu.py
@@ -23,7 +23,10 @@ class Osu:
             embed= discord.Embed(colour=self.bot.colour_for(ctx.author))
             embed.title=f"osu! user - {results[0].username}"
             embed.add_field(name="User Info", value=f"User ID: {results[0].user_id}\nLevel: {results[0].level}\nCountry: {results[0].country}\nAccuracy: {results[0].accuracy}",inline=True)
-            embed.add_field(name="User Stats", value=f"PP: {results[0].pp_raw}\nGlobal Rank: #{results[0].pp_rank}\nCountry Rank: {results[0].pp_country_rank}\n{results[0].count_rank_ss+results[0].count_rank_ssh} SS-ranked plays, {results[0].count_rank_s+results[0].count_rank_sh} S-ranked plays, {results[0].count_rank_a} A-ranked plays",inline=True)
+            ss = results[0].count_rank_ss+results[0].count_rank_ssh
+            s = results[0].count_rank_s+results[0].count_rank_sh
+            a = results[0].count_rank_a
+            embed.add_field(name="User Stats", value=f"PP: {results[0].pp_raw}\nGlobal Rank: #{results[0].pp_rank}\nCountry Rank: {results[0].pp_country_rank}\n{ss} SS-ranked play{'s' if ss != 1 else ''}, {s} S-ranked play{'s' if s != 1 else ''},\n{a} A-ranked play{'s' if a != 1 else ''}",inline=True)
             embed.set_footer(text=f"Total Plays: {results[0].playcount}")
             await ctx.send(embed=embed)
 
