2175039096a801527020138956bf8f58a9dd0f16
Starwort <tcphone93@gmail.com>
2018-12-22 17:01:22 +0000

    Need to fix that

--- lib/converters.py
+++ lib/converters.py
@@ -2,10 +2,10 @@ from discord.ext import commands
 import discord
 class osu_player_or_user(commands.Converter):
     async def convert(self,ctx,argument):
-        return (await commands.MemberConverter.convert(self,ctx,argument)) or (await commands.UserConverter.convert(self,ctx,argument)) or argument
+        return (await commands.MemberConverter().convert(ctx,argument)) or (await commands.UserConverter().convert(ctx,argument)) or argument
 class player(commands.Converter):
     async def convert(self,ctx,argument):
-        return ctx.bot.username_for(await commands.MemberConverter.convert(self,ctx,argument)) or ctx.bot.username_for(await commands.UserConverter.convert(self,ctx,argument)) or argument or ctx.bot.username_for(ctx.author)
+        return ctx.bot.username_for(await commands.MemberConverter().convert(ctx,argument)) or ctx.bot.username_for(await commands.UserConverter().convert(ctx,argument)) or argument or ctx.bot.username_for(ctx.author)
 class target(commands.Converter):
     async def convert(self,ctx,argument):
-        return (await commands.MemberConverter.convert(self,ctx,argument)) or (await commands.UserConverter.convert(self,ctx,argument)) or ctx.author
+        return (await commands.MemberConverter().convert(ctx,argument)) or (await commands.UserConverter().convert(ctx,argument)) or ctx.author
