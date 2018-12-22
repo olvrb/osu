b5dc7dd2da47d6d2957fdf88d16e49404e6e971f
Starwort <tcphone93@gmail.com>
2018-12-22 16:55:51 +0000

    Jfc fix that

--- cogs/meta.py
+++ cogs/meta.py
@@ -6,18 +6,18 @@ class Meta:
         self.bot = bot
 
     @commands.command(name = "ping", aliases = ['latency'])
-    ''' Get the bot latency. '''
     async def latency(self, ctx):
+        ''' Get the bot latency. '''
         return await ctx.send(f"⏱ `{round(self.bot.latency*1000)}ms`")
 
     @commands.command(name="invite")
-    ''' Get a link to invite osu!bot into your server. '''
     async def invite(self, ctx):
+        ''' Get a link to invite osu!bot into your server. '''
         return await ctx.send(':inbox_tray: Invite osu!bot to your server: https://discordapp.com/oauth2/authorize?client_id=421879566265614337&scope=bot&permissions=347136')
 
     @commands.command(name="support", aliases = ['server'])
-    ''' Get a link to the support server. '''
     async def support(self, ctx):
+        ''' Get a link to the support server. '''
         return await ctx.send(':grey_question: Join the support server here: discord.gg/uda4VuE')
 
 def setup(bot):
