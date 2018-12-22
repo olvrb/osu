faacb10ea7a04fff05bc714c62553ea56cb9d092
Starwort <tcphone93@gmail.com>
2018-12-22 17:04:33 +0000

    Fix converters

--- lib/converters.py
+++ lib/converters.py
@@ -2,10 +2,34 @@ from discord.ext import commands
 import discord
 class osu_player_or_user(commands.Converter):
     async def convert(self,ctx,argument):
-        return (await commands.MemberConverter().convert(ctx,argument)) or (await commands.UserConverter().convert(ctx,argument)) or argument
+        try:
+            Mc = await commands.MemberConverter().convert(ctx,argument)
+        except:
+            Mc = None
+        try:
+            Uc = await commands.UserConverter().convert(ctx,argument)
+        except:
+            Uc = None
+        return Mc or Uc or argument
 class player(commands.Converter):
     async def convert(self,ctx,argument):
-        return ctx.bot.username_for(await commands.MemberConverter().convert(ctx,argument)) or ctx.bot.username_for(await commands.UserConverter().convert(ctx,argument)) or argument or ctx.bot.username_for(ctx.author)
+        try:
+            Mc = await commands.MemberConverter().convert(ctx,argument)
+        except:
+            Mc = None
+        try:
+            Uc = await commands.UserConverter().convert(ctx,argument)
+        except:
+            Uc = None
+        return ctx.bot.username_for(Mc or Uc or argument)
 class target(commands.Converter):
     async def convert(self,ctx,argument):
-        return (await commands.MemberConverter().convert(ctx,argument)) or (await commands.UserConverter().convert(ctx,argument)) or ctx.author
+        try:
+            Mc = await commands.MemberConverter().convert(ctx,argument)
+        except:
+            Mc = None
+        try:
+            Uc = await commands.UserConverter().convert(ctx,argument)
+        except:
+            Uc = None
+        return Mc or Uc or ctx.author
